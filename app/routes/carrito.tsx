560d7369348aa3e35aa3dfba22c76e3967a3bbbe
Sandrixs <sandratondo@gmail.com>
2025-02-04 22:17:06 +0100

    style10

--- app/routes/carrito.tsx
+++ app/routes/carrito.tsx
@@ -17,57 +17,49 @@ const Carrito: React.FC = () => {
     return (
       <div className='h-all'>
         <Navegador />
-        <div className="container p-4 carrito-container">
+        <div className='container container-plus'>
           <div className="text-gray-600 text-m">CARRITO ({totalItems})</div>
           {carrito.length === 0 ? (
             <p className="text-gray-600">Tu carrito está vacío.</p>
           ) : (
-            <div>
-            <div className='mt-m'>
-              <div className='carrito-content'>
-                {carrito.map((item) => (
-                  <div key={`${item.id}-${item.color}-${item.storage}`} className="carrito-item p-4">
-                    <img src={item.imageUrl} alt={item.name} className="w-32 h-32 object-cover" />
-                      <div className='grid-row'>
-                        <div className="details flex-1">
-                          <p className="text-sm">{item.name}</p>
-                          <p className="text-sm"> {item.storage} | {item.color}</p>
-                          <p className="text-sm margin-bottom margin-bottom">{item.price} EUR</p>
-                        </div>
-                        <div className="mt-4">
-                          <button
-                            onClick={() => removeFromCarrito(item.id, item.color, item.storage)}
-                            className="bg-none text-sm text-red"
-                          >
-                            Eliminar
-                          </button>
-                        </div>
+            <div className='flexi'>
+              <div className="container p-4 carrito-container">
+                <div className='mt-m'>
+                  <div className='carrito-content'>
+                    {carrito.map((item) => (
+                      <div key={`${item.id}-${item.color}-${item.storage}`} className="carrito-item p-4">
+                          <img src={item.imageUrl} alt={item.name} className="w-32 h-32 object-cover" />
+                          <div className='grid-row'>
+                            <div className="details flex-1">
+                              <p className="text-sm">{item.name}</p>
+                              <p className="text-sm"> {item.storage} | {item.color}</p>
+                              <p className="text-sm margin-bottom margin-bottom">{item.price} EUR</p>
+                            </div>
+                            <div className="mt-4">
+                              <button onClick={() => removeFromCarrito(item.id, item.color, item.storage)} className="bg-none text-sm text-red">
+                                Eliminar
+                              </button>
+                            </div>
+                          </div>
                       </div>
+                    ))}
                   </div>
-                ))}
+                </div>
+                </div>
+                <div className="actions">
+                <div className="actions-buttons">
+                  <button
+                    onClick={redirigirPrincipal}
+                    className="button-secondary no-border"
+                  >
+                    CONTINUAR COMPRANDO
+                  </button>
+                </div>
+                <div className="total">
+                  <p><span className="total mx">Total</span> {totalPrecio} EUR</p>
+                </div>
               </div>
-
-            </div>
-
-<div className="actions">
-       
-<button
-  onClick={redirigirPrincipal}
-  className="button-secondary no-border"
->
-  CONTINUAR COMPRANDO
-</button>
-
-<div className="total">
-<p><span className="total total-m mx">Total</span> {totalPrecio} EUR</p>
-<button className="btn-main-black">PAGAR</button>
-</div>
-
-</div>
-
-</div>
-
-
+           </div> 
           )}
         </div>
       </div>
